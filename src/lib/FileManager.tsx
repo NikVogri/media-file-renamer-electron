091fbe29fe0b4eb7dad43f28dae33e8791c554e7
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-08-18 23:07:27 +0200

    fix: files can now be moved to other disks

--- src/lib/FileManager.tsx
+++ src/lib/FileManager.tsx
@@ -1,7 +1,9 @@
 import fs from 'fs-extra';
+import path from 'path';
 import ptt from 'parse-torrent-title';
-import { MAPPABLE_STRINGS } from '../config';
 import { Path } from './Path';
+
+import { MAPPABLE_STRINGS } from '../config';
 
 export class FileManager {
   public name: string;
@@ -21,15 +23,15 @@ export class FileManager {
   public size: number;
 
   constructor(file: File) {
-    const { name, path } = file;
+    const { name, path: filePath } = file;
 
     this.name = name;
     this.newName = name;
 
     this.size = file.size;
 
-    this.path = new Path(path);
-    this.newPath = new Path(path);
+    this.path = new Path(filePath);
+    this.newPath = new Path(filePath);
 
     const filenameData = this.parseFileName(file.name);
     this.filenameData = filenameData;
@@ -37,24 +39,30 @@ export class FileManager {
     this.type = file.type;
   }
 
-  public renameAndMove() {
-    const oldPath = `${this.path.build()}${this.name}`;
-    const newPath = `${this.newPath.build()}${this.newName}`;
+  public async renameAndMove() {
+    const oldPath = `${this.path.build()}${path.sep}${this.name}`;
+    const newPath = `${this.newPath.build()}${path.sep}${this.newName}`;
 
-    fs.renameSync(oldPath, newPath); // TODO: check implementation
+    await fs.move(oldPath, newPath);
+    // await fs.remove(oldPath);
+    // await fs.rename(oldPath, newPath); // TODO: check implementation
   }
 
   public applyTemplate(template: string) {
     let filledTemplate = template;
 
-    MAPPABLE_STRINGS.forEach((mappableStr: string) => {
-      filledTemplate = this.applyValueToTemplate(mappableStr, filledTemplate);
+    MAPPABLE_STRINGS.forEach((ms: string) => {
+      filledTemplate = this.applyValueToTemplate(ms, filledTemplate);
     });
 
-    this.edited = true;
-    console.log(filledTemplate);
-    this.newName = filledTemplate.split('/').pop() as string;
+    const originalExtension = this.name.split('.').pop() as string;
+    const newFilenameWithoutExtension = filledTemplate
+      .split('/')
+      .pop() as string;
+
+    this.newName = `${newFilenameWithoutExtension}.${originalExtension}`;
     this.newPath = new Path(filledTemplate);
+    this.edited = true;
   }
 
   private parseFileName = (name: string) => {
