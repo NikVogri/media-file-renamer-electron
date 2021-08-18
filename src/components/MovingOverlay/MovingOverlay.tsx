5fd98a91e7b07a2e373943e94cdb817b1aa244e9
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-08-18 23:04:01 +0200

    feat: moving overlay for different file move stages

--- src/components/MovingOverlay/MovingOverlay.tsx
+++ src/components/MovingOverlay/MovingOverlay.tsx
@@ -1,10 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './MovingOverlay.module.scss';
 
 import Spinner from '../../../assets/icons/circle-notch.svg';
+import CheckmarkIcon from '../../../assets/icons/checkmark.svg';
+import FailureIcon from '../../../assets/icons/failure.svg';
+import { GlobalContext } from '../../contexts/GlobalContext';
+import { MovingStep } from '../../lib/tsDefinitions';
 
-const MovingOverlay: React.FC = () => {
+interface MovingOverlayInterface {
+  type: MovingStep;
+  failReason?: string;
+  filesMoved?: string[];
+}
+
+const MovingOverlay: React.FC<MovingOverlayInterface> = ({
+  type,
+  failReason,
+}) => {
+  const { files, clearFiles, setFilesMoveStep } = useContext(GlobalContext);
   const [loadingDots, setLoadingDots] = useState('.');
 
   const updateDots = () => {
@@ -15,17 +30,73 @@ const MovingOverlay: React.FC = () => {
   };
 
   useEffect(() => {
-    const dotsInterval = setInterval(updateDots, 750);
+    let dotsInterval: NodeJS.Timeout;
+
+    if (type === MovingStep.loading) {
+      dotsInterval = setInterval(updateDots, 750);
+    }
 
     return () => clearInterval(dotsInterval);
-  }, []);
+  }, [type]);
 
-  return (
-    <div className={styles.backdrop}>
-      <img src={Spinner} alt="Spinner" />
-      <p>Moving files{loadingDots}</p>
-    </div>
-  );
+  const handleSuccessCloseOverlay = () => {
+    clearFiles();
+    setFilesMoveStep(MovingStep.none);
+  };
+
+  if (type === MovingStep.loading) {
+    return (
+      <div className={styles.backdrop}>
+        <img
+          src={Spinner}
+          alt="Spinner"
+          className={`${styles.spin} ${styles.icon}`}
+        />
+        <p>
+          <small>
+            This might take a few minutes, depending on the file size
+          </small>
+        </p>
+        <p>Moving files{loadingDots}</p>
+      </div>
+    );
+  }
+
+  if (type === MovingStep.success) {
+    return (
+      <div className={styles.backdrop}>
+        <img src={CheckmarkIcon} alt="Success" className={styles.icon} />
+        <p>Successfully moved {files.length} files</p>
+        <button type="button" onClick={() => handleSuccessCloseOverlay()}>
+          Close
+        </button>
+      </div>
+    );
+  }
+
+  if (type === MovingStep.fail) {
+    return (
+      <div className={styles.backdrop}>
+        <img src={FailureIcon} alt="Failure" className={styles.icon} />
+        <p>Moving failed</p>
+        <p>{failReason}</p>
+        <button type="button" onClick={() => setFilesMoveStep(MovingStep.none)}>
+          Close
+        </button>
+      </div>
+    );
+  }
+
+  return <></>;
+};
+
+MovingOverlay.propTypes = {
+  type: PropTypes.any.isRequired,
+  failReason: PropTypes.string,
+};
+
+MovingOverlay.defaultProps = {
+  failReason: undefined,
 };
 
 export default MovingOverlay;
