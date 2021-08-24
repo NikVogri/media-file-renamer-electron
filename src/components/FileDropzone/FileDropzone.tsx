32e7626d69e4771769ba23032003ee88b88e5327
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-08-24 22:54:05 +0200

    feat: FileDropzone refactor & better styles

--- src/components/FileDropzone/FileDropzone.tsx
+++ src/components/FileDropzone/FileDropzone.tsx
@@ -9,7 +9,7 @@ import dropItemsIcon from '../../../assets/icons/drop-items.svg';
 
 import styles from './FileDropzone.module.scss';
 
-const Dropdown: React.FC = () => {
+const FileDropzone: React.FC = () => {
   const dispatch = useDispatch();
 
   const onDrop = useCallback(
@@ -19,23 +19,42 @@ const Dropdown: React.FC = () => {
     },
     [dispatch]
   );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div {...getRootProps()} className={styles.dropzone}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <div>active</div>
-      ) : (
-        <div className={styles.dropzoneCenter}>
-          <img src={dropItemsIcon} alt="Drop files or folders" />
-          <p>
-            <strong>Drag & drop</strong> files or folders here
-          </p>
-        </div>
-      )}
+
+      <div className={styles.dropzoneCenter}>
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          viewBox="0 0 16 16"
+          width="100"
+          height="100"
+          className={styles.icon}
+        >
+          <title>Drop items</title>
+          <g className="nc-icon-wrapper">
+            <path
+              className={styles.iconArrow}
+              d="M8,12c0.3,0,0.5-0.1,0.7-0.3L14.4,6L13,4.6l-4,4V0H7v8.6l-4-4L1.6,6l5.7,5.7C7.5,11.9,7.7,12,8,12z"
+            />{' '}
+            <path
+              className={styles.iconBase}
+              d="M14,14H2v-3H0v4c0,0.6,0.4,1,1,1h14c0.6,0,1-0.4,1-1v-4h-2V14z"
+            />
+          </g>
+        </svg>
+        <p>
+          <strong>Drag & drop</strong> files or folders here
+        </p>
+      </div>
     </div>
   );
 };
 
-export default Dropdown;
+export default FileDropzone;
