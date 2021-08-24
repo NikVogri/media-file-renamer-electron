a9065eb4e0e7ef763974c6c039a296001aaec3f2
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-08-24 22:54:23 +0200

    feat: FileListItem refactor & better styles

--- src/components/FileListItem/FileListItem.tsx
+++ src/components/FileListItem/FileListItem.tsx
@@ -4,8 +4,6 @@ import path from 'path';
 
 import styles from './FileListItem.module.scss';
 
-import videoIcon from '../../../assets/icons/video.svg';
-import subtitleIcon from '../../../assets/icons/subtitles.svg';
 import { FileManager } from '../../lib/FileManager';
 import { isVideo } from '../../lib/fileHelpers';
 
@@ -14,15 +12,40 @@ interface FileListItemProps {
   showEdited: boolean;
 }
 
+const videoIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+  >
+    <title>ic_movie_creation_48px</title>
+    <g className="nc-icon-wrapper" fill="#1C81F3">
+      <path d="M36 8l4 8h-6l-4-8h-4l4 8h-6l-4-8h-4l4 8h-6l-4-8H8c-2.21 0-3.98 1.79-3.98 4L4 36c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V8h-8z" />
+    </g>
+  </svg>
+);
+
+const subtitleIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+  >
+    <title>ic_subtitles_48px</title>
+    <g className="nc-icon-wrapper" fill="#1C81F3">
+      <path d="M40 8H8c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V12c0-2.21-1.79-4-4-4zM8 24h8v4H8v-4zm20 12H8v-4h20v4zm12 0h-8v-4h8v4zm0-8H20v-4h20v4z" />
+    </g>
+  </svg>
+);
+
 const FileListItem: React.FC<FileListItemProps> = ({ file, showEdited }) => {
   const fileIsVideo = isVideo(file.type);
 
   return (
     <li className={styles.file_list_item}>
-      <img
-        src={fileIsVideo ? videoIcon : subtitleIcon}
-        alt={fileIsVideo ? 'Video Icon' : 'Subtitle Icon'}
-      />
+      {fileIsVideo ? videoIcon : subtitleIcon}
 
       <div className={styles.info}>
         <p>
