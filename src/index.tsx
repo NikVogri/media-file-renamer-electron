53070308851f58e5d6d0dff11e200c469027858b
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-09-02 21:20:54 +0200

    feat: added redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import App from './App';
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <div id="modal" />
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,34 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import fileReducer from './reducers/fileReducer';
 import settingsReducer from './reducers/settingsReducer';
+import uiReducer from './reducers/uiReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['settings'],
+};
 
 const rootReducer = combineReducers({
   file: fileReducer,
   settings: settingsReducer,
+  ui: uiReducer,
 });
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
 const middleware = [thunk, logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(
+  persistedRootReducer,
+  applyMiddleware(...middleware)
+);
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
