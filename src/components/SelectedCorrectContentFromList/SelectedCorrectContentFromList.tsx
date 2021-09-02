fcc8de1b0fb7bd3b7dc735feb61f86a2fc2e4c7e
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-09-02 21:33:18 +0200

    feat: modal that allows user to choose which fetched item is correct for that file

---
+++ src/components/SelectedCorrectContentFromList/SelectedCorrectContentFromList.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addFetchedDataToFile } from '../../redux/actions/fileActionsCreator';
+import BaseModal from '../BaseModal/BaseModal';
+
+import styles from './SelectedCorrectContentFromList.module.scss';
+
+interface SelectedCorrectContentFromListProps {
+  show: boolean;
+  setShow: () => void;
+  fetchResults: unknown[];
+}
+
+const SelectedCorrectContentFromList: React.FC<SelectedCorrectContentFromListProps> = ({
+  show,
+  setShow,
+  fetchResults,
+}) => {
+  const dispatch = useDispatch();
+  const handleSelect = (fetchResult: any) => {
+    dispatch(addFetchedDataToFile(fetchResult));
+  };
+
+  return (
+    <BaseModal show={show} setShow={setShow}>
+      <div className={styles.inner}>
+        <h3>Select One:</h3>
+        <ul>
+          {fetchResults.map((fr: any) => (
+            <button onClick={() => handleSelect(fr)} type="button" key={fr.id}>
+              <li>
+                {fr.posterPath ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200/${fr.posterPath}`}
+                    alt={fr.title}
+                  />
+                ) : (
+                  <div className={styles.noImage} />
+                )}
+                <div>
+                  <h5>{fr.title}</h5>
+                  <span>{fr.releaseDate}</span>
+                </div>
+              </li>
+            </button>
+          ))}
+        </ul>
+      </div>
+    </BaseModal>
+  );
+};
+
+export default SelectedCorrectContentFromList;
